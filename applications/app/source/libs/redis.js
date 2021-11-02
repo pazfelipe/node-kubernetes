d56a26ea7f1d954d85d9617d1584d407ce3191f5
Felipe Paz <xxfelipepaz@gmail.com>
2021-11-02 14:35:04 -0300

    feat: added redis

---
+++ applications/app/source/libs/redis.js
@@ -0,0 +1,46 @@
+const redis = require('redis');
+const { promisify } = require('util');
+class RedisClient {
+
+  #port;
+  #host;
+  #connection;
+  constructor() {
+    this.#host = process.env.REDIS_HOST;
+    this.#port = process.env.REDIS_PORT;
+
+    this.#connection = redis.createClient(this.#port, this.#host);
+  }
+
+  #connect() {
+    return this.#connection;
+  }
+
+  get client() {
+    return this.#connection;
+  }
+
+  async keys(pattern) {
+    return new Promise((resolve, reject) => {
+      this.client.keys(pattern, (err, result) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(result);
+      });
+    });
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, result) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(result);
+      });
+    });
+  }
+}
+
+module.exports = RedisClient;
